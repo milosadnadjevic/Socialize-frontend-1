b7b97648b647f528b65c24991b4bd8b907690143
Milos Adnadjevic <milos@Miloss-Air.lan>
2023-10-19 13:57:18 -0400

    myPostWidget.jsx added

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from '@mui/material'
 import Navbar from 'scenes/navbar'
 import UserWidget from 'scenes/widgets/UserWidget';
 import { useSelector } from 'react-redux'
+import MyPostWidget from 'scenes/widgets/MyPostWidget';
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
@@ -26,9 +27,11 @@ const HomePage = () => {
         flexBasis={isNonMobileScreens ? '42%' : undefined }
         mt={isNonMobileScreens ? undefined : '2rem'}
         >
+          <MyPostWidget picturePath={picturePath} />
+        
 
 
-      
+
         </Box>
       </Box>
     </Box>
